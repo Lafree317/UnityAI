4d513e45fef6b8f4d2b6d505edcac2a209876e9c
LafreeZhaZha <lafree317@163.com>
2017-09-10 20:01:48 +0800

    CreateProject
    分层寻路系统

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Transform findGoal;
+    private NavMeshAgent _agent;
+
+	// Use this for initialization
+	void Start () {
+        _agent = this.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_agent && findGoal) {
+            _agent.SetDestination(findGoal.transform.position);
+        }
+	}
+}
